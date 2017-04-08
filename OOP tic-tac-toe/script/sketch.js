04ef1520ba4e8e8e3bf4db0f6ad185312f21b606
Adil Iqbal <aiqbal85@gmail.com>
2017-04-08 00:42:38 -0500

    Create sketch.js

---
+++ OOP tic-tac-toe/script/sketch.js
@@ -0,0 +1,46 @@
+var games = [];
+var human = -1;
+var empty = 0;
+var computer = 1;
+var winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
+function setup() {
+    createCanvas(830, 220);
+    noLoop();
+    rectMode(CENTER);
+    for (var i = 0; i <= 3; i++) {
+        var size = floor(random(25, 200));
+        var x = ((size * 1.167) / 2) + 10
+        if (i > 0) {
+            x += games[i - 1].bounds.posX
+        }
+        games[i] = new Board(i, x, 110, size);
+        games[i].initialize();
+        console.log((i + 1) + '. ' + floor((1-games[i].blunder)*100))
+    }
+    redraw();
+}
+
+
+function draw() {
+    for (var i = 0; i < games.length; i++) {
+        games[i].show()
+    }
+}
+
+
+function mousePressed() {
+    for (var i = 0; i < games.length; i++) {
+        games[i].clicked()
+    }
+}
