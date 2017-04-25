e1178da8269b99e2058153ca28448801cc01dd88
Adil Iqbal <aiqbal85@gmail.com>
2017-04-25 01:25:05 -0500

    Minor bug fixes

--- Random Sequence/rseq.py
+++ Random Sequence/rseq.py
@@ -5,7 +5,7 @@ from random import randint, randrange
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 
-def randomSeq(size=None, language=IUPAC.unambiguous_dna):
+def randomSeq(size=None, lang=IUPAC.unambiguous_dna):
     valid_langs =  [IUPAC.unambiguous_dna,
                     IUPAC.extended_protein,
                     IUPAC.protein,
@@ -15,28 +15,27 @@ def randomSeq(size=None, language=IUPAC.unambiguous_dna):
                     IUPAC.unambiguous_rna]
                     
     # If no size parameter was declared, generate it randomly.
-    if size == None: 
+    if size == None:
         size = randrange(3, 100, 3)
         
-    # Validate language parameter.
+    # Validate parameters.
+    try:
+        size = int(size)
+        size -= size % 3
+    except:
+        print('Size parameter of \'randomSeq\' must be an integer.')
+        
     for i in range(len(valid_langs)):
-        if language == valid_langs[i]:
+        if lang == valid_langs[i]:
             break
     else:
         print('Language parameter of \'randomSeq\' must be a valid IUPAC alphabet.')
         raise TypeError
         
-    # Validate size parameter.
-    try:
-        size = int(size - (size % 3))
-    except:
-        print('Size parameter of \'randomSeq\' must be an integer.')
-        raise TypeError
-        
     # Generate random sequence.
     gene = ""
     for i in range(size):
-        roll = randint(0, len(language.letters) - 1)
-        gene += language.letters[roll]
+        roll = randint(0, len(lang.letters) - 1)
+        gene += lang.letters[roll]
         
-    return Seq(gene, language)
+    return Seq(gene, lang)
