65e570d7ed5f259c8d9ffc2d9154e682379b875a
Adil Iqbal <aiqbal85@gmail.com>
2017-06-10 23:48:32 -0500

    Major update

--- Test Sequence/test_testseq.py
+++ Test Sequence/test_testseq.py
@@ -1,62 +1,83 @@
+# Copyright 2017 by Adil Iqbal.
+# All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+import os
+import sys
 import unittest
-from Scripts.testseq import testseq
+
 from Bio.Alphabet import IUPAC, NucleotideAlphabet
 from Bio.SeqUtils import GC
 
+# Find file path to 'testseq' module.
+file_path = os.getcwd()
+cwd_path = file_path
+file_path = os.path.split(file_path)
+if file_path[1] == "Tests":
+    file_path = os.path.join(file_path[0], "Scripts")
+    file_path = os.path.join(file_path, "testseq.py")
+else:
+    raise ImportError("File path could not be derived from: " + cwd_path)
+
+# Import module manually
+if sys.version_info[0] < 3:
+    # Python version 2.7
+    import imp
+    foo = imp.load_source('testseq', file_path)
+else:
+    if sys.version_info[1] <= 4:
+        # Python version 3.3 and 3.4
+        from importlib.machinery import SourceFileLoader
+        foo = SourceFileLoader('testseq', file_path).load_module()
+    else:
+        # Python version 3.5+
+        from importlib.util import spec_from_file_location, module_from_spec
+        spec = spec_from_file_location('testseq', file_path)
+        foo = module_from_spec(spec)
+        spec.loader.exec_module(foo)
+
 
 class TestTestseq(unittest.TestCase):
+    """Perform unit test for 'testseq' function."""
 
     def test_alphabet(self):
-        seq = testseq()
-        seq = seq._data
-        self.assertEqual(seq, "ATGTCCTCTAATAGTATGGTCGTCTACTGA")
+        """Testing 'alphabet' argument..."""
+        alphabets = [IUPAC.unambiguous_dna, IUPAC.ambiguous_dna, IUPAC.extended_dna,
+                     IUPAC.unambiguous_rna, IUPAC.ambiguous_rna, IUPAC.protein, IUPAC.extended_protein]
 
-        seq = testseq(alphabet=IUPAC.ambiguous_dna)
-        seq = seq._data
-        self.assertEqual(seq, "ATGNWGRMSWNVDRSYKNNCMTRTVAKTRA")
-
-        seq = testseq(alphabet=IUPAC.extended_dna)
-        seq = seq._data
-        self.assertEqual(seq, "ATGBWSBWDCTBTABTBAADWADSDCWTGA")
-
-        seq = testseq(alphabet=IUPAC.unambiguous_rna)
-        seq = seq._data
-        self.assertEqual(seq, "AUGUCCUCUAAUAGUAUGGUCGUCUACUGA")
-
-        seq = testseq(alphabet=IUPAC.ambiguous_rna)
-        seq = seq._data
-        self.assertEqual(seq, "AUGNWGRMSWNVDRSYKNNCMURUVAKURA")
-
-        seq = testseq(alphabet=IUPAC.protein)
-        seq = seq._data
-        self.assertEqual(seq, "MQCKTSPLSNWHTFLFEYKVYFLEDMSVE*")
-
-        seq = testseq(alphabet=IUPAC.extended_protein)
-        seq = seq._data
-        self.assertEqual(seq, "MUQCKTSPOLSNWHTFLFUEYOKVZOYFL*")
+        for i in alphabets:
+            seq = foo.testseq(alphabet=i)._data
+            for j, k in enumerate(seq):
+                self.assertTrue(k in i.letters)
 
         with self.assertRaises(TypeError):
-            testseq(alphabet=NucleotideAlphabet)
+            foo.testseq(alphabet=NucleotideAlphabet)
 
     def test_size(self):
-        seq = testseq()
+        """Testing 'size' and 'truncate' arguments..."""
+        seq = foo.testseq()
         self.assertEqual(len(seq), 30)
 
-        seq = testseq(100) 
+        seq = foo.testseq(100)
         self.assertEqual(len(seq), 99)
 
-        seq = testseq(100, alphabet=IUPAC.protein)
+        seq = foo.testseq(100, alphabet=IUPAC.protein)
         self.assertEqual(len(seq), 100)
 
-        seq = testseq(100, truncate=False)
+        seq = foo.testseq(100, truncate=False)
         self.assertEqual(len(seq), 100)
 
     def test_gc_target(self):
-        seq = testseq(1000, gc_target=90)
-        self.assertEqual(GC(seq), 89.48948948948949)
+        """Testing 'gc_target' argument..."""
+        seq = foo.testseq(1000, gc_target=90)
+        error = 90 - GC(seq)
+        self.assertTrue(-5 < error < 5)
 
     def test_codon_tables(self):
-        seq = testseq(table=5)
+        """Testing codon sets..."""
+        seq = foo.testseq(table=5)
         seq1 = seq.translate(table=5)._data
         seq2 = seq.translate(table=6)._data
 
@@ -65,7 +86,7 @@ class TestTestseq(unittest.TestCase):
         self.assertTrue(seq1[-1] == "*")
         self.assertFalse(seq2[-1] == "*")
 
-        seq = testseq(size=1000, from_start=False, to_stop=False, persistent=False)
+        seq = foo.testseq(size=1000, from_start=False, to_stop=False, persistent=False)
         seq = seq.translate()._data
 
         self.assertFalse(seq[0] == "M")
@@ -73,18 +94,21 @@ class TestTestseq(unittest.TestCase):
         self.assertTrue("*" in seq[1:-1])
 
     def test_messenger(self):
-        seq = testseq(alphabet=IUPAC.unambiguous_rna, messenger=True)._data
+        """Testing 'messenger' argument..."""
+        seq = foo.testseq(alphabet=IUPAC.unambiguous_rna, messenger=True)._data
         self.assertTrue("A" * 20 == seq[-20:])
 
     def test_seeding(self):
-        seq1 = testseq(rand_seed=None)
-        seq2 = testseq(rand_seed=None)
+        """Testing 'rand_seed' argument..."""
+        seq1 = foo.testseq(rand_seed=None)
+        seq2 = foo.testseq(rand_seed=None)
         self.assertFalse(seq1 == seq2)
 
-        seq1 = testseq(rand_seed=50)
-        seq2 = testseq(rand_seed=50)
+        seq1 = foo.testseq(rand_seed=50)
+        seq2 = foo.testseq(rand_seed=50)
         self.assertTrue(seq1 == seq2)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
