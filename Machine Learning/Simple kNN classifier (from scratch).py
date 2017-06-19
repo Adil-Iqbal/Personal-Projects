e6502490861d0cfdc66689e3dc3209a69db03bce
Adil-Iqbal <aiqbal85@gmail.com>
2017-06-19 02:27:25 -0500

    Refactor

--- Machine Learning/Simple kNN classifier (from scratch).py
+++ Machine Learning/Simple kNN classifier (from scratch).py
@@ -1,6 +1,33 @@
+# Copyright 2017 by Adil Iqbal.
+# All rights reserved.
+
+"""Use k-Nearest Neighbors Algorithm to predict Iris dataset."""
+
 from scipy.spatial import distance
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+def main():
+    """Build, train, and test kNN."""
+    iris = datasets.load_iris()
+    x = iris.data  # Define features.
+    y = iris.target  # Define targets.
+    # Partition data into training and testing sets.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+    classifier = Scrappy_kNN()  # Instantiate classifier.
+    classifier.fit(x_train, y_train)  # Train classifier.
+    prediction = classifier.predict(x_test)  # Test classifier.
+    # Determine accuracy of predictions.
+    result = accuracy_score(y_test, prediction)
+    print(result)
+
+
 def euc(a, b):
+    """Return euclidean distance between a & b."""
     return distance.euclidean(a, b)
+
 
 class Scrappy_kNN():
     """kNN classifier (k=1)."""
@@ -27,30 +54,9 @@ class Scrappy_kNN():
             if dist < best_dist:
                 best_dist = dist
                 best_index = i
-        return y_train[best_index]
+        return self.y_train[best_index]
 
-# Import data
-from sklearn import datasets
-iris = datasets.load_iris()
 
-# Define features and targets.
-x = iris.data
-y = iris.target
-
-# Partition data into training and testing sets.
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
-
-# Instantiate classifier.
-classifier = Scrappy_kNN()
-
-# Train and test data.
-classifier.fit(x_train, y_train)
-prediction = classifier.predict(x_test)
-
-# Determine accuracy of predictions.
-from sklearn.metrics import accuracy_score
-result = accuracy_score(y_test, prediction)
-
-# Print accuracy of predictions.
-print(result)
+if __name__ == '__main__':
+    main()
+    exit()
